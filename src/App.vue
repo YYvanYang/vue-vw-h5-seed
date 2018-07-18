29a448ce0adab6010f3364c794cc84f4aa2f2797
Yvan Yang <yvan.yang@ulsee.com>
2018-07-18 11:59:09 +0800

    update style

--- src/App.vue
+++ src/App.vue
@@ -11,23 +11,22 @@ export default {
 </script>
 
 <style>
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+
 html {
   margin: 0;
   padding: 0;
   height: 100vh;
-  box-sizing: border-box;
 }
 body {
   margin: 0;
   padding: 0;
   min-height: 100vh;
   background-color: #f4f4f4;
-}
-
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
 }
 
 img {
